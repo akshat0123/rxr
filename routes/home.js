bc907a5da218c84c9aaed95f9feb265aa7f03643
Akshat Pandey <akshat0123@gmail.com>
2015-07-25 00:12:28 -0400

    First commit

---
+++ routes/home.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('home');
+});
+
+router.get('/propaganda', function(req, res, next) {
+	res.render('propaganda');
+});
+
+router.get('/gear', function(req, res, next) {
+	res.render('gear');
+});
+
+router.get('/blog', function(req, res, next) {
+	res.render('blog');
+});
+
+router.get('/cart', function(req, res, next) {
+	res.render('cart');
+});
+
+router.get('/login', function(req, res, next) {
+	res.render('login');
+});
+
+module.exports = router;
