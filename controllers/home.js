3348e956e678938a7da05a2e593f0a2209944646
Akshat Pandey <akshat0123@gmail.com>
2015-08-11 15:26:07 -0400

    sessions work. logout is now included

--- controllers/home.js
+++ controllers/home.js
@@ -1,35 +1,44 @@
 var express = require('express');
 var router = express.Router();
 
+var sess;
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
 	req.session.username = null;
-  res.render('home');
+	sess = req.session;
+  res.render('home', { session: sess});
 });
 
 router.get('/propaganda', function(req, res, next) {
-	console.log('USERNAME (propaganda): ' + req.session.username);
-	res.render('propaganda');
+	sess = req.session;
+	res.render('propaganda', { session: sess });
 });
 
 router.get('/gear', function(req, res, next) {
-	console.log('USERNAME (gear): ' + req.session.username);
-	res.render('gear');
+	sess = req.session;
+	res.render('gear', { session: sess });
 });
 
 router.get('/blog', function(req, res, next) {
-	console.log('USERNAME (blog): ' + req.session.username);
-	res.render('blog');
+	sess = req.session;
+	res.render('blog', { session: sess });
 });
 
 router.get('/cart', function(req, res, next) {
-	console.log('USERNAME (cart): ' + req.session.username);
-	res.render('cart');
+	sess = req.session;
+	res.render('cart', { session: sess });
 });
 
 router.get('/login', function(req, res, next) {
-	console.log('USERNAME (login): ' + req.session.username);
-	res.render('login', { message: null } );
+	sess = req.session;
+	res.render('login', { message: null, session: sess } );
+});
+
+router.get('/logout', function(req, res, next) {
+	req.session.username = null;
+	sess = req.session;
+	res.render('home', { session: sess});
 });
 
 module.exports = router;
--- controllers/users.js
+++ controllers/users.js
@@ -3,18 +3,20 @@ var router = express.Router();
 
 var db = require('../models/user');
 
+var sess;
+
 router.post('/addUser', function(req, res) {
 	var firstname = req.body.firstname;
 	var lastname = req.body.lastname;
 	var email = req.body.newuseremail;
 	var password = req.body.newuserpassword;
 	var retypepassword = req.body.retypepassword;
+	sess = req.session;
 	if (password !== retypepassword) {
 		res.render('login', { message: 'Passwords don\'t match' } );
 	} else {
 		db.addUser(firstname, lastname, email, password, function(result) {
-			console.log('USERNAME (addUser): ' + req.session.username);
-			res.render('home');
+			res.render('home', { session: sess });
 		});
 	}
 });
@@ -22,11 +24,11 @@ router.post('/addUser', function(req, res) {
 router.post('/login', function(req, res) {
 	var email = req.body.olduseremail;
 	var password = req.body.olduserpassword;
+	sess = req.session;
 	db.login(email, password, function(result) {
 		if (result === true) { 
 			req.session.username = email;
-			console.log('USERNAME (login): ' + req.session.username);
-			res.render('home'); 
+			res.render('home', { session: sess }); 
 		}
 		else { res.render('login', { message: 'No such account'}); }
 	});
