82599faf48886d118830cc044ec7f0797e957d27
Akshat Pandey <akshat0123@gmail.com>
2015-08-12 13:53:43 -0400

    Sessions done - currently no passport authentication

--- controllers/home.js
+++ controllers/home.js
@@ -11,34 +11,34 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/propaganda', function(req, res, next) {
-	sess = req.session;
+	sess = req.session
 	res.render('propaganda', { session: sess });
 });
 
 router.get('/gear', function(req, res, next) {
-	sess = req.session;
+	sess = req.session
 	res.render('gear', { session: sess });
 });
 
 router.get('/blog', function(req, res, next) {
-	sess = req.session;
+	sess = req.session
 	res.render('blog', { session: sess });
 });
 
 router.get('/cart', function(req, res, next) {
-	sess = req.session;
+	sess = req.session
 	res.render('cart', { session: sess });
 });
 
 router.get('/login', function(req, res, next) {
-	sess = req.session;
+	sess = req.session
 	res.render('login', { message: null, session: sess } );
 });
 
 router.get('/logout', function(req, res, next) {
-	req.session.username = null;
-	sess = req.session;
-	res.render('home', { session: sess});
+	sess.username = null;
+	sess = req.session
+	res.render('home', { session: req.session});
 });
 
 module.exports = router;
